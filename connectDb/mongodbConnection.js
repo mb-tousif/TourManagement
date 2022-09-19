0a9d39f870a9ca27c6a56d5e83d246c15dac771f
AZIZUL HAQUE TOUSIF <meta.block.tousif@gmail.com>
2022-09-19 11:27:54 +0600

    Repo Initialized with Mongodb connection

---
+++ connectDb/mongodbConnection.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const connect = async (req, res) => {
+  const url = `mongodb+srv://${process.env.MONGODB_USER_NAME}:${process.env.MONGODB_USER_PASSWORD}@tourmanagement.bx0yeko.mongodb.net/TourManagement`;
+  
+
+  try {
+    await mongoose.connect(url, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+    console.log("Mongodb connected");
+  } catch (error) {
+    console.log("Error while connecting with Mongodb", error.message);
+  }
+};
+
+export default connect;
---
+++ index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import connect from "./connectDb/mongodbConnection.js";
+import Routes from "./Router/Route.js";
+const app = express();
+const port = process.env.PORT || 4000;
+
+dotenv.config();
+app.use(cors(config()));
+
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+connect();
+
+app.use("/api", Routes);
+
+app.get("/", (req, res) => {
+  res.send("Vercel is ready to use!");
+});
+
+app.listen(port, (req, res) => {
+  console.log("Welcome to server");
+});
